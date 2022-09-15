016193f616fca1626c5be34dfb9a79cfc50483ab
Clayderman Rojas <cj.rojasj1@uniandes.edu.co>
2022-09-14 21:49:46 -0500

    Implementación de la lógica base para supermercado

--- src/supermercado/supermercado.service.ts
+++ src/supermercado/supermercado.service.ts
@@ -1,4 +1,46 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { BusinessError, BusinessLogicException } from '../shared/errors/business-errors';
+import { Repository } from 'typeorm';
+import { SupermercadoEntity } from './supermercado.entity';
 
 @Injectable()
-export class SupermercadoService {}
+export class SupermercadoService {
+    constructor(
+        @InjectRepository(SupermercadoEntity)
+        private readonly supermercadoRepository: Repository<SupermercadoEntity>
+    ){}
+
+    async findAll(): Promise<SupermercadoEntity[]> {
+        return await this.supermercadoRepository.find({ relations: ["artworks", "exhibitions"] });
+    }
+
+    async findOne(id: string): Promise<SupermercadoEntity> {
+        const supermercado: SupermercadoEntity = await this.supermercadoRepository.findOne({where: {id}, relations: ["artworks", "exhibitions"] } );
+        if (!supermercado)
+          throw new BusinessLogicException("El supermercado con el ID dado no fue encontrado", BusinessError.NOT_FOUND);
+    
+        return supermercado;
+    }
+    
+    async create(supermercado: SupermercadoEntity): Promise<SupermercadoEntity> {
+        return await this.supermercadoRepository.save(supermercado);
+    }
+
+    async update(id: string, supermercado: SupermercadoEntity): Promise<SupermercadoEntity> {
+        const persistedSupermercado: SupermercadoEntity = await this.supermercadoRepository.findOne({where:{id}});
+        if (!persistedSupermercado)
+          throw new BusinessLogicException("El supermercado con el ID dado no fue encontrado", BusinessError.NOT_FOUND);
+        
+        return await this.supermercadoRepository.save({...persistedSupermercado, ...supermercado});
+    }
+
+    async delete(id: string) {
+        const supermercado: SupermercadoEntity = await this.supermercadoRepository.findOne({where:{id}});
+        if (!supermercado)
+          throw new BusinessLogicException("El supermercado con el ID dado no fue encontrado", BusinessError.NOT_FOUND);
+      
+        await this.supermercadoRepository.remove(supermercado);
+    }
+}
