7d29a7c29351a550b8477145c965edbaff7d7990
Clayderman Rojas <cj.rojasj1@uniandes.edu.co>
2022-09-14 21:45:33 -0500

    Se agrega lógica para ciudad más manejo de errores

--- src/ciudad/ciudad.service.ts
+++ src/ciudad/ciudad.service.ts
@@ -1,4 +1,46 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { BusinessError, BusinessLogicException } from 'src/shared/errors/business-errors';
+import { Repository } from 'typeorm';
+import { CiudadEntity } from './ciudad.entity';
 
 @Injectable()
-export class CiudadService {}
+export class CiudadService {
+    constructor(
+        @InjectRepository(CiudadEntity)
+        private readonly ciudadRepository: Repository<CiudadEntity>
+    ){}
+
+    async findAll(): Promise<CiudadEntity[]> {
+        return await this.ciudadRepository.find({ relations: ["artworks", "exhibitions"] });
+    }
+
+    async findOne(id: string): Promise<CiudadEntity> {
+        const ciudad: CiudadEntity = await this.ciudadRepository.findOne({where: {id}, relations: ["artworks", "exhibitions"] } );
+        if (!ciudad)
+          throw new BusinessLogicException("La ciudad con el ID dado no fue encontrada", BusinessError.NOT_FOUND);
+    
+        return ciudad;
+    }
+    
+    async create(ciudad: CiudadEntity): Promise<CiudadEntity> {
+        return await this.ciudadRepository.save(ciudad);
+    }
+
+    async update(id: string, ciudad: CiudadEntity): Promise<CiudadEntity> {
+        const persistedCiudad: CiudadEntity = await this.ciudadRepository.findOne({where:{id}});
+        if (!persistedCiudad)
+          throw new BusinessLogicException("La ciudad con el ID dado no fue encontrada", BusinessError.NOT_FOUND);
+        
+        return await this.ciudadRepository.save({...persistedCiudad, ...ciudad});
+    }
+
+    async delete(id: string) {
+        const ciudad: CiudadEntity = await this.ciudadRepository.findOne({where:{id}});
+        if (!ciudad)
+          throw new BusinessLogicException("La ciudad con el ID dado no fue encontrada", BusinessError.NOT_FOUND);
+      
+        await this.ciudadRepository.remove(ciudad);
+    }
+}
