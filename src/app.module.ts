567df973d45d56ee7b722c70b3e89755ffbb717f
Clayderman Rojas <cj.rojasj1@uniandes.edu.co>
2022-09-14 21:26:58 -0500

    Se agrega persistencia en memoria

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,21 @@ import { AppService } from './app.service';
 import { CiudadModule } from './ciudad/ciudad.module';
 import { SupermercadoModule } from './supermercado/supermercado.module';
 import { SupermercadoCiudadModule } from './supermercado-ciudad/supermercado-ciudad.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CiudadEntity } from './ciudad/ciudad.entity';
+import { SupermercadoEntity } from './supermercado/supermercado.entity';
 
 @Module({
-  imports: [CiudadModule, SupermercadoModule, SupermercadoCiudadModule],
+  imports: [CiudadModule, SupermercadoModule, SupermercadoCiudadModule,
+  TypeOrmModule.forRoot({
+    type: 'sqlite',
+    database: ':memory:',
+    dropSchema: true,
+    entities: [CiudadEntity, SupermercadoEntity],
+    synchronize: true,
+    keepConnectionAlive: true
+  }),
+],  
   controllers: [AppController],
   providers: [AppService],
 })
